e018b581b1dbe2e8e4dce2813501ffda94652547
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-01-12 16:19:42 -0600

    style: display navbar css

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Post(models.Model):
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    # feature_image =models.ImageField(null=True, blank=True)
+    # STATUS_CHOICES = (
+    # ('draft', 'Draft'),
+    # ('published', 'Published'),
+    # )
+    #status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
 
-# Create your models here.
