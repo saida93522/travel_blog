a73fff9391808db9dae49d7d9e8670d06eb6ce40
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-01-28 12:08:28 -0600

    refactor: blog html clean up

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('blog/',views.blog, name='blog')
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request,'blog/home.html',context)
+
+def blog(request):
+    context = {}
+    return render(request,'blog/blog.html',context)
