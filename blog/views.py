fa359f18d329af8330a57066e74018d006b48f0d
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-01 16:09:32 -0600

    feat: display post using pagination

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render,redirect,get_object_or_404,reverse
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-
-from .models import Author,Country,Post
+from .models import Author, Country, Post
 
 # Create your views here.
 def home(request):
@@ -12,8 +12,11 @@ def home(request):
 
 def blog(request):
     articles = Post.objects.prefetch_related('country')
+    # articles = Post.objects.all()
     featured = Post.objects.filter(is_featured=True)
-    context = {'featured':featured,'articles':articles}
+
+    
+    context = {'featured':featured,'articles':paginated_page,'page_req':page_req}
     return render(request,'blog/blog.html',context)
 
 def post(request,pk):
