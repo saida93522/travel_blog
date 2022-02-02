57b843714c2c7268e40cf4ef8b7fdfd197e68750
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-02 16:26:58 -0600

    test: test homepage contains quote

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Author, Post, Country 
+
+class TestHomePage(TestCase):
+    def test_hero_quote_shown_home_page(self):
+        " test hopepage hero section shows quotes "
+        url = reverse('home')
+
+        # object client
+        response = self.client.get(url)
+        self.assertContains(response, '”A journey of a thousand miles starts with a single step.” – LaoTz')
+    
