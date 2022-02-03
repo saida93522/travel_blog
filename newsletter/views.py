ebfe452f9f0a0d33c41b2cf67fe7e11983dede92
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-03 09:50:40 -0600

    feat: set up newsletter template

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .forms import SubscribersForm, NewsLetterForm
 # Create your views here.
+def sign_up(request):
+    """ validates subscribers email
+    """
+    form = SubscribersForm()
+
+    context = {}
+    return render(request, 'blog/subscribe.html',context)
+
+
+def news_letter(request):
+    context = {}
+    return render(request, 'blog/newsletter.html',context)
+    
