3e414c618910cc6f9075898939d14ac549559bea
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-21 14:27:44 -0600

    test:add test to Author model for model _str_ representation.

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -6,17 +6,21 @@ from django.contrib.auth.models import User
 from .. import models
 
 class TestAuthor(TestCase):
-    def test_author_creation(self):
-        """Test one to one relationship
-        """
-        user = User.objects.create_superuser(
+    def setUp(self):
+        self.user = User.objects.create_superuser(
             username='bob', 
             email='bob@bobby.com',
             password='admin')
-        user.save()
-        #Create author 
-        author1 = models.Author(author=user,philo='my travel philosophy..')
-        author1.save()
-        #assert Author is instance of user
-        self.assertIsInstance(user.author, models.Author)
+        self.author1 = models.Author(
+            author=self.user,
+            philo='my travel philosophy..')
         
+    def test_author_creation(self):
+        """Test one to one relationship
+        """
+        #assert Author is instance of user
+        self.assertIsInstance(self.user.author, models.Author)
+
+    def test_str_representation(self):
+         """Test author model returns string representation"""
+        self.assertEqual(self.author1.__str__(), self.author1.author.username)
