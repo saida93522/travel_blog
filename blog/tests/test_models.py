0162be611e2efa69a8d78d327d5514c6535aaa32
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-21 12:05:54 -0600

    test:add test for Author creation.

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,4 +1,22 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from blog.models import Author, Post, Country
+from django.contrib.auth.models import User
+# from blog.models import Author, Post, Country
+from .. import models
+
+class TestAuthor(TestCase):
+    def test_author_creation(self):
+        """Test one to one relationship
+        """
+        user = User.objects.create_superuser(
+            username='bob', 
+            email='bob@bobby.com',
+            password='admin')
+        user.save()
+        #Create author 
+        author1 = models.Author(author=user,philo='my travel philosophy..')
+        author1.save()
+        #assert Author is instance of user
+        self.assertIsInstance(user.author, models.Author)
+        
