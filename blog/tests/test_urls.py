e5d2d64e9c659ec5e8ce0326820e3265ebdfac21
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-22 10:37:17 -0600

    docs:add spacing between test functions

--- blog/tests/test_urls.py
+++ blog/tests/test_urls.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 
-from django.contrib.auth.models import User
-from .. import models
 from .. import views
 
 class TestURLS(TestCase):
@@ -13,10 +11,9 @@ class TestURLS(TestCase):
         response = resolve(url).func
         self.assertEqual(response, views.home)
 
-
     def test_blog_url_resolves(self):
         """ Test url resolves and returns blog url. """
-
+        
         url = reverse('blog')
         response = resolve(url).func
         self.assertEqual(response, views.blog)
@@ -24,7 +21,7 @@ class TestURLS(TestCase):
 
     def test_post_url_resolves(self):
         """ Test url resolves and returns post url. """
-
+        
         # set url to resolve of post,id
         url = reverse('post', kwargs={'pk':'ad45g39f'})
         response = resolve(url).func
@@ -37,7 +34,6 @@ class TestURLS(TestCase):
         self.assertNotEqual(response, views.blog)
         self.assertNotEqual([resolve(url).kwargs,resolve(url2).kwargs], views.post)
 
-
     def test_create_post_url_resolves(self):
         """ Test url resolves and returns create_post url. """
         
@@ -48,7 +44,7 @@ class TestURLS(TestCase):
         
     def test_update_post_url_resolves(self):
         """ Test url resolves and returns update_post url. """
-
+        
         url = reverse('update_post', kwargs={'pk':'ad45g39f'})
         # set url to resolve of post,id
         response = resolve(url).func
@@ -61,9 +57,9 @@ class TestURLS(TestCase):
         self.assertNotEqual(response, views.create_post)
         self.assertNotEqual([resolve(url).kwargs,resolve(url2).kwargs], views.update_post)
 
-
     def test_delete_post_urls_resolves(self):
         """ Test url resolves and returns delete_post url. """
+        
         url = reverse('delete_post', kwargs={'pk':'a756kgk'})
         response = resolve(url).func
         self.assertEqual(response, views.delete_post)
@@ -71,15 +67,16 @@ class TestURLS(TestCase):
 
     def test_about_url_resolves(self):
         """ Test url resolves and returns about url. """
+        
         url = reverse('about')
         response = resolve(url).func
         self.assertEqual(response, views.about)
         self.assertNotEqual(response, views.home)
         self.assertNotEqual(response, views.blog)
 
-
     def test_newsletter_url_resolves(self):
         """ Test url resolves and returns newsletter url. """
+        
         url = reverse('news_letter')
         response = resolve(url).func
         self.assertEqual(response,views.news_letter)
@@ -89,6 +86,7 @@ class TestURLS(TestCase):
 
     def test_search_url_resolves(self):
         """ Test url resolves and returns search url. """
+        
         url = reverse('search')
         response = resolve(url).func
         self.assertEqual(response,views.search)
