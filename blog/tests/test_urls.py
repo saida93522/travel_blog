808e39ba42302fb53974bac0ecc1c5e63e9de164
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-22 09:48:31 -0600

    test: add test 2 different path path for poth to resolve post url

--- blog/tests/test_urls.py
+++ blog/tests/test_urls.py
@@ -6,23 +6,41 @@ from .. import models
 from .. import views
 
 class TestURLS(TestCase):
-    def test_home_urls_is_resolved(self):
-        # set url to resolve of home
+    def test_home_urls_is_resolves(self):
+        """ Test url resolves and returns blog url. """
+        
         url = reverse('home')
         response = resolve(url).func
         self.assertEqual(response, views.home)
 
 
-    def test_blog_urls_is_resolved(self):
-        # set url to resolve of home
+    def test_blog_urls_is_resolves(self):
+        """ Test url resolves and returns blog url. """
+
         url = reverse('blog')
         response = resolve(url).func
         self.assertEqual(response, views.blog)
         self.assertNotEqual(response, views.home)
 
+    def test_post_urls_is_resolves(self):
+        """ Test url resolves and returns post url. """
 
-    def test_create_post_urls_is_resolved(self):
-        # set url to resolve of home
+        # set url to resolve of post,id
+        url = reverse('post', kwargs={'pk':'ad45g39f'})
+        response = resolve(url).func
+        self.assertEqual(response, views.post)
+
+        url2 = reverse('post', kwargs={'pk':'dc78339d'})
+        response2 = resolve(url2).func
+ 
+        self.assertNotEqual(response, views.home)
+        self.assertNotEqual(response, views.blog)
+        self.assertNotEqual([resolve(url).kwargs,resolve(url2).kwargs], views.post)
+
+
+    def test_create_post_urls_is_resolves(self):
+        """ Test url resolves and returns create_page url. """
+        
         url = reverse('create_post')
         response = resolve(url).func
         self.assertEqual(response, views.create_post)
