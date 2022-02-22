c6272ede7f34d2a5eb366f2e17a66e2fe7e534d9
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-22 10:15:31 -0600

    test: add test for delete_post url resolves

--- blog/tests/test_urls.py
+++ blog/tests/test_urls.py
@@ -6,7 +6,7 @@ from .. import models
 from .. import views
 
 class TestURLS(TestCase):
-    def test_home_urls_is_resolves(self):
+    def test_home_urls_resolves(self):
         """ Test url resolves and returns blog url. """
         
         url = reverse('home')
@@ -14,7 +14,7 @@ class TestURLS(TestCase):
         self.assertEqual(response, views.home)
 
 
-    def test_blog_urls_is_resolves(self):
+    def test_blog_urls_resolves(self):
         """ Test url resolves and returns blog url. """
 
         url = reverse('blog')
@@ -22,7 +22,7 @@ class TestURLS(TestCase):
         self.assertEqual(response, views.blog)
         self.assertNotEqual(response, views.home)
 
-    def test_post_urls_is_resolves(self):
+    def test_post_urls_resolves(self):
         """ Test url resolves and returns post url. """
 
         # set url to resolve of post,id
@@ -38,10 +38,33 @@ class TestURLS(TestCase):
         self.assertNotEqual([resolve(url).kwargs,resolve(url2).kwargs], views.post)
 
 
-    def test_create_post_urls_is_resolves(self):
-        """ Test url resolves and returns create_page url. """
+    def test_create_post_urls_resolves(self):
+        """ Test url resolves and returns create_post url. """
         
         url = reverse('create_post')
         response = resolve(url).func
         self.assertEqual(response, views.create_post)
+        self.assertNotEqual(response, views.update_post)
+        
+    def test_update_post_urls_resolves(self):
+        """ Test url resolves and returns update_post url. """
+
+        url = reverse('update_post', kwargs={'pk':'ad45g39f'})
+        # set url to resolve of post,id
+        response = resolve(url).func
+        self.assertEqual(response, views.update_post)
+
+        url2 = reverse('update_post', kwargs={'pk':'dc78339d'})
+        response2 = resolve(url2).func
+ 
+        self.assertNotEqual(response, views.blog)
+        self.assertNotEqual(response, views.create_post)
+        self.assertNotEqual([resolve(url).kwargs,resolve(url2).kwargs], views.update_post)
+
+
+    def test_delete_post_urls_resolves(self):
+        """ Test url resolves and returns delete_post url. """
+        url = reverse('delete_post', kwargs={'pk':'a756kgk'})
+        response = resolve(url).func
+        self.assertEqual(response, views.delete_post)
         self.assertNotEqual(response, views.update_post)
