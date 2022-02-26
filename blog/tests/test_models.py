d00b7e602e6933cdcc1f316c9cf6c8069610e95d
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-26 09:23:53 -0600

    test for model str representation

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -4,28 +4,35 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 # from blog.models import Author, Post, Country
 from .. import models
+from .test_views import BlogDataTestCase
 
-class TestAuthor(TestCase):
+
+class TestBlogModel(BlogDataTestCase):
     def setUp(self):
-        self.user = User.objects.create_superuser(
-            username='bob', 
-            email='bob@bobby.com',
-            password='admin')
-        self.author1 = models.Author(
-            author=self.user,
-            philo='my travel philosophy..')
-        
-    def test_author_creation(self):
+        super().setUp()
+      
+    def test_instance_creation(self):
         """Test one to one relationship
         """
-        #assert Author is instance of user
+        #assert objects instance of models
         self.assertIsInstance(self.user.author, models.Author)
+        self.assertIsInstance(self.country1, models.Country)
+        self.assertIsInstance(self.post2, models.Post)
+        
 
     def test_str_representation(self):
-         """Test author model returns string representation"""
+        """Test author model returns string representation"""
         self.assertEqual(self.author1.__str__(), self.author1.author.username)
+        self.assertEqual(self.post1.__str__(), self.post1.owner.author.username)
+        self.assertEqual(self.country1.__str__(), self.country1.name)
+
+    def test_auto_field_datetime(self):
+        post = models.Post.objects.get(pk=self.post2.pk)
+        self.assertIsNotNone(post.created_at)
+        old_time = post.created_at
+        post.title = 'Test time updates'
+        post.save()
+        self.assertTrue(post.created_at == old_time)
 
 
 
-class TestPostModel(TestCase):
-    pass
