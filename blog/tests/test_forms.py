ace7c4e6925f15b8654d3b18893f2f06108d593f
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-02-26 09:59:17 -0600

    test: test for form validation

--- blog/tests/test_forms.py
+++ blog/tests/test_forms.py
@@ -1,4 +1,37 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .forms import PostForm, SubscribersForm, NewsLetterForm, CommentForm
+from blog.forms import PostForm, SubscribersForm, NewsLetterForm, CommentForm
+from .test_views import BlogDataTestCase
+from .. import models
+
+class TestBlogForm(BlogDataTestCase):
+    def setUp(self):
+        super().setUp()
+
+    def test_comment(self):
+        post = models.Post.objects.get(pk=self.post3.pk)
+        data = {'name':'tester',
+                'email':'test@email',
+                'content':'test body',
+                'user_img':models.Comment.user_img}
+        
+        response = self.client.post(reverse('post',kwargs={'pk':post.pk}),data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'tester')
+        self.assertContains(response, 'test body')
+
+    def test_missing_email_is_invalid(self):
+        form_data = {'name':'test'}
+        form = CommentForm(form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_subscribe_email_is_valid(self):
+        form_data = {'email':'testing@test.com'}
+        form = SubscribersForm(form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_newsletter_title_and_message_is_valid(self):
+        form_data = {'title':'Testing Title','message':'test message'}
+        form = NewsLetterForm(form_data)
+        self.assertTrue(form.is_valid())
