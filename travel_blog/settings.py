0df43d427c3ca0eded460187e6413aa5e95846ee
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-03-02 11:27:16 -0600

    build: config db

--- travel_blog/settings.py
+++ travel_blog/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tinywanderlust-blog-saida.herokuapp.com']
+ALLOWED_HOSTS = ['tinywanderlust-blog-saida.herokuapp.com','localhost']
 
 
 # Application definition
